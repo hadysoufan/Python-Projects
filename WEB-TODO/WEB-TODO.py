f653b17e142807c2aead10014c8d5ba89d194a4f
hadysoufan <hadisoufan74@gmail.com>
2023-06-11 18:30:05 +0300

    Implementing the checkout TODO functionality

--- WEB-TODO/WEB-TODO.py
+++ WEB-TODO/WEB-TODO.py
@@ -12,8 +12,14 @@ def add_todo():
 
 st.title('My TODO App')
 
-for todo in todos:
-    st.checkbox(todo)
+for i, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+    if checkbox:
+        todos.pop(i)
+        web_functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label='', placeholder='Add new TODO',
               on_change=add_todo, key='new_todo')
