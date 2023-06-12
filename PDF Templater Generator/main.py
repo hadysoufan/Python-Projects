57fb8b6c148cf9e5600a756e8eb6e6ae12f29b55
hadysoufan <hadisoufan74@gmail.com>
2023-06-12 18:29:09 +0300

    Creating Script for generating pdf from .csv files

--- PDF Templater Generator/main.py
+++ PDF Templater Generator/main.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 
 def footer():
+    """
+    Add a footer to the PDF document.
+
+    Draws a horizontal line at the bottom of the page and adds the 'Topic' text
+    in italics at the right-aligned position.
+    """
     pdf.ln(265)
     pdf.set_font(family='Times', style='I', size=8)
     pdf.set_text_color(180, 180, 180)
@@ -12,9 +18,18 @@ def footer():
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
-df = pd.read_csv('topics.csv ')
+df = pd.read_csv('topics.csv')
 
 for i, row in df.iterrows():
+    """
+    Iterate over each row in the DataFrame and generate a PDF page for each topic.
+
+    - Add a new page to the PDF document.
+    - Set the font style, size, and color for the topic title.
+    - Draw a horizontal line below the topic title.
+    - Add a footer to the page.
+    - If the 'Pages' column value is greater than 1, add additional pages with the same footer.
+    """
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
